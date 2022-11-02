56a74639e79721b3e7d87ddad71c775bc8ab4fd7
dibdas <dibyandu08das@gmail.com>
2022-11-03 02:06:17 +0530

    modified

---
+++ src/reducer.js
@@ -0,0 +1,36 @@
+ function reducer(state,action){
+
+   console.log("make it happen in reducer")
+  console.log({state,action})
+  if(action.type==="DECREASE")
+  {
+    // console.log("hey it works")
+    // state.count= state.count-1 // it wont work because we are mutating the state directly without making a copy of it and then returning
+    //return{count:state.count-1} // making a copy of the old state and returning that new state
+    // ...state it means copying  and pasting the old state value , not mutating
+    // {...state,count:state.count+1} // copying and pasting the old state , 
+    // and changing the state what I desire 
+    // {...state,count:state.count+1} // here john is coming from the old state as well as the new state
+    // where as return{count:state.count-1} john is not coming in the new state
+  //   return{...state,count:state.count-1} 
+
+  }
+  if(action.type==="RESET"){
+    //return{count:state.count=65}
+    //OR 
+    return{...state,count:65}
+  }
+  if(action.type==="INCREASE"){
+    return{...state,count:state.count+1}
+  }
+  if(action.type==="CHANGE_NAME"){
+    return{...state,name:"santa"}
+  }
+  
+  // return state // by default we should always return the old state
+  //return "shake and bake" // we can return anything from the reducer , but functinality may change
+   return state
+}
+
+export default reducer;
+
